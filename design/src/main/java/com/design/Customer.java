f6f6909ec4b6fd0ba7c1d3682399aa6f85f292c0
SkyNaris <uradoncenko0@gmail.com>
2025-12-01 13:55:04 +0000

    Робота з базою даних із використанням впровадження залежностей через setter-метод класу

--- design/src/main/java/com/design/Customer.java
+++ design/src/main/java/com/design/Customer.java
@@ -1,52 +1,41 @@
 package com.design;
 
-import com.google.inject.Inject; // Додаємо імпорт для Guice
+import com.google.inject.Inject;
 
-/**
- * Клас замовника
- */
 public class Customer extends Person {
     private Brief brief;
-    private final DesignService designService; // Поле для сервісу БД
+    private DesignService designService; 
     
-    /**
-     * Конструктор класу Customer з ін'єкцією залежностей
-     * @param designService сервіс для роботи з БД (надає Guice)
-     */
+    // 3.1. Змінюємо конструктор:
+    // Прибираємо @Inject і прибираємо DesignService з параметрів.
+    // Тепер це звичайний конструктор.
+    public Customer() {
+        super("Замовник"); 
+    }
+    
+    // 3.2. Додаємо Setter Method для впровадження залежності
+    // Guice знайде цей метод завдяки @Inject і викличе його одразу після створення об'єкта
     @Inject
-    public Customer(DesignService designService) {
-        super("Замовник"); // Тимчасове ім'я, справжнє встановимо в Main через setName
+    public void setDesignService(DesignService designService) {
         this.designService = designService;
     }
     
-    /**
-     * Метод заповнення брифу
-     * @return заповнений бриф
-     */
     public Brief fillBrief() {
-        // Оскільки ім'я тепер може бути змінене через сетер, беремо актуальне
         System.out.println("Замовник " + getName() + " заповнює бриф");
-        
         brief = new Brief("Деталі проєкту");
         
-        // --- ЗБЕРЕЖЕННЯ В БД ---
-        // Викликаємо метод сервісу, щоб зберегти створений бриф
-        designService.saveBrief(brief);
-        // -----------------------
+        // Перевірка на null, про всяк випадок (хоча Guice гарантує впровадження)
+        if (designService != null) {
+            designService.saveBrief(brief);
+        }
         
         return brief;
     }
     
-    /**
-     * Метод прийняття концептів
-     */
     public void acceptConcepts() {
         System.out.println("Замовник " + getName() + " приймає концепти");
     }
     
-    /**
-     * Метод погодження правок
-     */
     public void approveEdits() {
         System.out.println("Замовник " + getName() + " погоджує правки");
     }
